4d1bf93663da3116268ee6d2be79995a3e682e2d
Mateo Tilves Freijeiro <mateotd2@gmail.com>
2023-08-28 05:54:56 +0200

    Configurado SwaggerUI con popup de Bearer Token TG-1

---
+++ harvest-proyect/harvest-backend/src/main/java/com/udc/fic/swaggerconfig/OpenApi30Config.java
@@ -0,0 +1,20 @@
+package com.udc.fic.swaggerconfig;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "HarvestApi", version = "v1", contact = @Contact(name = "Mateo Tilves Freijeiro", email = "mateo.tilves@udc.es")
+        , summary = "SWAGGER UI para la documentacion del API REST"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi30Config {
+}
