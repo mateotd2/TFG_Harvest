4ac116f18d423c9865db11a65cb1b14125aa0da3
Mateo Tilves Freijeiro <mateotd2@gmail.com>
2023-12-11 13:02:43 +0100

    Inicio de iteracion 3 frontend TG-9

--- harvest-frontend/lib/UI/home_pages/admin_pages/admin.dart
+++ harvest-frontend/lib/UI/home_pages/admin_pages/admin.dart
@@ -4,6 +4,8 @@ import 'package:harvest_frontend/UI/home_pages/admin_pages/worker_pages/pasar_li
 import 'package:harvest_frontend/UI/home_pages/admin_pages/worker_pages/workers.dart';
 import 'package:logger/logger.dart';
 
+import 'lines_pages/zonas.dart';
+
 class Admin extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _AdminState();
@@ -19,7 +21,7 @@ class _AdminState extends State<Admin> {
     });
   }
 
-  final List _paginasAdmin = [Emps(), Trabajadores(), PasarLista()];
+  final List _paginasAdmin = [Emps(), Trabajadores(), Zonas(), PasarLista()];
 
   @override
   Widget build(BuildContext context) {
@@ -27,10 +29,13 @@ class _AdminState extends State<Admin> {
       key: Key('adminKey'),
       body: _paginasAdmin[_navIndice],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Empleados'),
           BottomNavigationBarItem(
               icon: Icon(Icons.groups_outlined), label: 'Operarios'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.view_column_rounded), label: 'Zonas'),
           BottomNavigationBarItem(
               icon: Icon(Icons.assignment_turned_in_outlined),
               label: 'Pasar Lista')
